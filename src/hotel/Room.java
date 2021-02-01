c48eec9bc33e6eeca9206fd7a354d4f8eca7b2ec
Lahey <oskar.franck@gmail.com>
2021-02-01 14:59:05 +0100

    Sync

---
+++ src/hotel/Room.java
@@ -0,0 +1,31 @@
+package hotel;
+
+public class Room {
+
+    private final int roomNumber;
+    private final RoomType roomType;
+    private boolean rented = false;
+    //private Customer renter = null;
+
+    Room(int roomNumber, RoomType roomType) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public boolean getRented() {
+        return rented;
+    }
+
+//    public Customer getRenter() {
+//        return renter;
+//    }
+
+}
