1813e1772a30d9419ce108b9f4e50cd80ce66854
Oscar Bergström <oscar@obstrom.com>
2021-02-02 12:05:44 +0100

    Fixed class name

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 	// write your code here
         MainMenu.EmployeeMenu();
-        Database db = new Database("EliteHotelDB", databaseCredentials.databaseUser, databaseCredentials.databasePassword);
+        Database db = new Database("EliteHotelDB", DatabaseCredentials.databaseUser, DatabaseCredentials.databasePassword);
     }
 
 }
