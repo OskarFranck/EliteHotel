106ad469386131d3a099e8c69081cd1717b8d707
Oscar Bergström <oscar@obstrom.com>
2021-02-02 12:35:22 +0100

    Added simple schema and tested database connection

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -6,8 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 	// write your code here
+        Database db = new Database("elitehotel", DatabaseCredentials.databaseUser, DatabaseCredentials.databasePassword);
+
         MainMenu.EmployeeMenu();
-        Database db = new Database("EliteHotelDB", DatabaseCredentials.databaseUser, DatabaseCredentials.databasePassword);
     }
 
 }
