9ab0eb730567a3e3ff25e8e2ef9c407102c148a2
Oscar Bergström <oscar@obstrom.com>
2021-02-03 12:32:29 +0100

    * Made database into a Singleton class. * Changed error print-outs to by System.err. * Added upgradeBooking() for upgrading the room of a current booking. * Added getBooking() to get a specific booking form an ID. * Added (private) noBookingExists() to easily check if a booking ID exists in the database.

--- src/hotel/Database.java
+++ src/hotel/Database.java
@@ -7,10 +7,18 @@ public class Database {
 
     private final String url;
     private Connection sqlConnection;
+    public static Database singletonInstance = null;
 
     Database(String databaseName, String databaseUser, String databasePassword) throws SQLException {
         this.url = "jdbc:mysql://localhost:3306/" + databaseName + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         this.sqlConnection = DriverManager.getConnection(url, databaseUser, databasePassword);
+    }
+
+    public static Database getInstance() throws SQLException {
+        if (singletonInstance == null) {
+            singletonInstance = new Database("elitehotel", DatabaseCredentials.databaseUser, DatabaseCredentials.databasePassword);
+        }
+        return singletonInstance;
     }
 
     public void close() throws SQLException {
@@ -31,7 +39,7 @@ public class Database {
             statement.executeUpdate();
             return true;
         } catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Customer already exists in database!");
+            System.err.println("Error: Customer already exists in database!");
             return false;
         }
     }
@@ -40,8 +48,6 @@ public class Database {
         return sqlConnection.createStatement().executeQuery("SELECT * FROM Room");
     }
 
-    /*
-    // TODO - Requires RoomType enum to run
     public boolean addRoom(int roomNumber, RoomType type) throws SQLException {
         try {
             PreparedStatement statement = sqlConnection.prepareStatement("INSERT INTO Room VALUES (?,?)");
@@ -50,17 +56,15 @@ public class Database {
             statement.executeUpdate();
             return true;
         } catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Room already exists in database!");
+            System.err.println("Error: Room already exists in database!");
             return false;
         }
     }
-     */
 
     public ResultSet getAllBookings() throws SQLException {
         return sqlConnection.createStatement().executeQuery("SELECT * FROM Booking");
     }
 
-    // TODO - Will this autoincrement in database?
     public boolean addBooking(int roomNumber, int customerId, LocalDate checkInDate) throws SQLException {
         try {
             PreparedStatement statement = sqlConnection.prepareStatement("INSERT INTO Booking (roomNumber, customerId, checkInDate) VALUES (?,?,?)");
@@ -70,9 +74,19 @@ public class Database {
             statement.executeUpdate();
             return true;
         } catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Room already exists in database!");
+            System.err.println("Error: Can not add booking to database!");
             return false;
         }
+    }
+
+    public ResultSet getBooking(int bookingId) throws SQLException {
+        PreparedStatement query = sqlConnection.prepareStatement("SELECT * FROM Booking WHERE bookingId = ?");
+        query.setInt(1, bookingId);
+        return query.executeQuery();
+    }
+
+    private boolean noBookingExists(int bookingId) throws SQLException {
+        return !getBooking(bookingId).next();
     }
 
     public boolean addBooking(int roomNumber, int customerId, LocalDate checkInDate, LocalDate checkOutDate) throws SQLException {
@@ -85,22 +99,48 @@ public class Database {
             statement.executeUpdate();
             return true;
         } catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Room already exists in database!");
+            System.err.println("Error: Can not add booking to database!");
+            return false;
+        }
+    }
+
+    public boolean upgradeBooking(int bookingId, int newRoomNumber) throws SQLException {
+        if (noBookingExists(bookingId)) {
+            System.err.println("Error: No booking with that ID!");
+            return false;
+        }
+
+        try {
+            PreparedStatement statement = sqlConnection.prepareStatement("UPDATE Booking SET roomNumber = ? WHERE bookingId = ?");
+            statement.setInt(1, newRoomNumber);
+            statement.setInt(2, bookingId);
+            return true;
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.err.println("Error: Can not update booking to database!");
             return false;
         }
     }
 
     public boolean checkOutBooking(int bookingId, LocalDate checkOutDate) throws SQLException {
-        PreparedStatement query = sqlConnection.prepareStatement("SELECT checkOutDate FROM Booking WHERE bookingId = ?");
-        // Tests if above query for checkOutBooking returns empty
-        if (!query.executeQuery().next()) {
+        if (noBookingExists(bookingId)) {
+            System.err.println("Error: No booking with that ID!");
+            return false;
+        }
+
+        // Get checkOutDate cell from database for this booking
+        ResultSet query = getBooking(bookingId);
+        query.next();
+        String checkOutDateCell = query.getString(5);
+
+        // Verify that a checkOutDate does not already exist for this booking
+        if (checkOutDateCell == null) {
             PreparedStatement statement = sqlConnection.prepareStatement("UPDATE Booking SET checkOutDate = ? WHERE bookingId = ?");
             statement.setDate(1, Date.valueOf(checkOutDate));
             statement.setInt(2, bookingId);
             statement.executeUpdate();
             return true;
         } else {
-            System.out.println("Booking #" + bookingId + " is already checked out!");
+            System.err.println("Error: Booking #" + bookingId + " is already checked out!");
             return false;
         }
     }
--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -6,7 +6,17 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 	// write your code here
-        Database db = new Database("elitehotel", DatabaseCredentials.databaseUser, DatabaseCredentials.databasePassword);
+
+        // Du når nu databasen genom att ropa på den genom "Database.getInstance()..."
+        // Database i sig är inte statisk, men du når den och alla metoder som om den vore det-
+        // -genom att använda .getInstance()
+
+        // Exempel:
+        // Database.getInstance().getAllCustomers();
+
+        // Första gången du ropar på .getInstance() så initsierar den kopplingen mot databasen.
+        // Mer om hur Singleton fungerar: https://www.geeksforgeeks.org/singleton-class-java/
+
         MainMenu.EmployeeMenu();
     }
 
